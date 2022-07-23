492f88b6197590f1f761a25a6285b4b91328271d
l-ricardo <oieusoulu@gmail.com>
2022-07-22 23:23:21 -0300

    testando o commit direto na master

--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
 print('Hello, esse é um teste do repositorio do trabalho final de apc')
 print('Faca um commit de uma nova linha aqui em baixo com seu nome se vc tem acesso a fazer alteracoes no codigo')
 print('André Em.')
+print('Ricardo tem acesso e esta testando o commit direto no master')
