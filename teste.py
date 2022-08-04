615d798af71953f8f91a1367d48a9088a495d7cc
Hunter104 <pudimdeleite2004@outlook.com>
2022-08-04 08:25:48 -0300

    Importado math

--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
+import math
 print('Hello, esse é um teste do repositorio do trabalho final de apc')
 print('Faca um commit de uma nova linha aqui em baixo com seu nome se vc tem acesso a fazer alteracoes no codigo')
 print('André Em.')
