6b20e536a3ed2a301ab02aa9d20ec64c875a6876
Hunter104 <pudimdeleite2004@outlook.com>
2022-08-09 22:06:02 -0300

    Adicionada função de retirar valores nulos

--- Dashboard-Oficial/src/mapa copy.py
+++ Dashboard-Oficial/src/mapa copy.py
@@ -1,7 +1,9 @@
+from cmath import isnan, nan
 import pandas as pd
 import plotly.express as  px
 import json
 import os
+import math
 
 #Estrutura do to_dict
 # {cabecalho:{id:valor}}
@@ -24,10 +26,28 @@ def filtrar(tabela: pd.DataFrame, filtros: list) -> pd.DataFrame:
     resultado_pd = pd.DataFrame(resultado)
     return resultado_pd
 
+def retirar_vazios(tabela):
+    linhas_a_retirar=[]
+    tabela=tabela.to_dict()
+    for cabecalho in tabela:
+        coluna=tabela[cabecalho]
+        for index in coluna:
+            if isinstance(coluna[index], float):
+                if math.isnan(coluna[index]) and not index in linhas_a_retirar:
+                    linhas_a_retirar.append(index)
+
+    for linha in linhas_a_retirar:
+        for cabecalho in tabela:
+            tabela[cabecalho].pop(linha)
+
+    return pd.DataFrame(tabela)
+
 estados_brasileiros = json.load(open('Dashboard-Oficial/data/brasil_estados.json'))
 voos = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep=';', encoding='latin')
 
 voos_por_estado = filtrar(voos,['AEROPORTO DE DESTINO (UF)','DECOLAGENS'])
+print(voos_por_estado)
+print(retirar_vazios(voos_por_estado))
 
 #print(filtrar_estados(voos_dict,['DECOLAGENS','AEROPORTO DE DESTINO (UF)']))
 #mapa = px.choropleth(dados_pd, 
