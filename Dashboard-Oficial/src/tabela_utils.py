48b4bb4554597fe495be44611a78f6b5086853a4
Hunter104 <pudimdeleite2004@outlook.com>
2022-08-09 23:19:06 -0300

    Atualizado módulo de utilidades

--- Dashboard-Oficial/src/tabela_utils.py
+++ Dashboard-Oficial/src/tabela_utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import math
+
 def filtrar(tabela: pd.DataFrame, filtros: list) -> pd.DataFrame: 
     """Função feita para filtrar colunas em Dataframes
 
@@ -8,10 +11,42 @@ def filtrar(tabela: pd.DataFrame, filtros: list) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Dataframe com as colunas filtradas
     """
-    resultado={}
     tabela = tabela.to_dict()
-    for cabecalho in  tabela:
-        if cabecalho in filtros:
-            resultado[cabecalho]=tabela[cabecalho]
-    resultado_pd = pd.DataFrame(resultado)
-    return resultado_pd
+    colunas_a_remover=[]
+
+    #Checar quais colunas não estão no filtro
+    for coluna in tabela:
+        if not coluna in filtros:
+            colunas_a_remover.append(coluna)
+        
+    #Remover todas as colunas com o nome na lista
+    for coluna in colunas_a_remover:
+        tabela.pop(coluna)
+    
+    #Transformar tabela em um dataframe novamente
+    return pd.DataFrame(tabela)
+
+
+def retirar_nulos(tabela: pd.DataFrame) -> pd.DataFrame:
+    """Retira todas as linhas que contenham qualquer valor NaN de uma tabela
+
+    Args:
+        tabela (pd.DataFrame): A tabela a ser operada
+
+    Returns:
+        pd.DataFrame: Uma tabela com os valores nulos retirados
+    """
+    linhas_a_retirar=[]
+    tabela=tabela.to_dict()
+    for cabecalho in tabela:
+        coluna=tabela[cabecalho]
+        for index in coluna:
+            if isinstance(coluna[index], float):
+                if math.isnan(coluna[index]) and not index in linhas_a_retirar:
+                    linhas_a_retirar.append(index)
+
+    for linha in linhas_a_retirar:
+        for cabecalho in tabela:
+            tabela[cabecalho].pop(linha)
+
+    return pd.DataFrame(tabela)
