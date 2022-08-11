b691bf7d7fbd5dae23914cb90b2da465907c048e
Hunter104 <pudimdeleite2004@outlook.com>
2022-08-11 09:44:57 -0300

    Valor máximo alterado

--- Dashboard-Oficial/src/mapa.py
+++ Dashboard-Oficial/src/mapa.py
@@ -11,12 +11,13 @@ voos = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep=';', enco
 voos_por_estado = tabela_utils.filtrar(voos,['AEROPORTO DE DESTINO (UF)','DECOLAGENS'])
 vpe_nao_nulos = tabela_utils.retirar_nulos(voos_por_estado)
 decolagens_por_estado = tabela_utils.soma_por_categoria(vpe_nao_nulos, 'AEROPORTO DE DESTINO (UF)', 'DECOLAGENS')
+maximo_decolagens = tabela_utils.maximo(decolagens_por_estado, 'DECOLAGENS')
 
 mapa = px.choropleth(decolagens_por_estado, 
                     geojson=estados_brasileiros, 
                     locations='AEROPORTO DE DESTINO (UF)', 
                     color='DECOLAGENS', 
-                    range_color=(0,200000), 
+                    range_color=(0,maximo_decolagens/4), 
                     hover_data =['AEROPORTO DE DESTINO (UF)'], 
                     scope='south america')
 mapa.show()
