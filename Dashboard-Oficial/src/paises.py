9d2588d72c9ad67617b41c1ca1b1f61ad15d1bce
Guilherme <guilhermecarmona10@gmail.com>
2022-08-12 21:15:20 -0300

    Filtrando as colunas

--- Dashboard-Oficial/src/paises.py
+++ Dashboard-Oficial/src/paises.py
@@ -3,6 +3,8 @@ import pandas as pd
 import tabela_utils
 
 
-tabela = pd.read_csv('C:\UnB\APC\DashboardAPC\Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep=';')
+tabela = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep=';', encoding='latin')
+filtro = tabela_utils.filtrar(tabela, ['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
 
-print(tabela.to_string())
+
+print(filtro.to_string())
