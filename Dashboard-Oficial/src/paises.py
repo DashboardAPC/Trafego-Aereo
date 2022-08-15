baadb62532ba50f3921060065352fbeedbf43315
Hunter104 <pudimdeleite2004@outlook.com>
2022-08-15 16:59:07 -0300

    Organizados dados

--- Dashboard-Oficial/src/paises.py
+++ Dashboard-Oficial/src/paises.py
@@ -3,8 +3,9 @@ import pandas as pd
 import tabela_utils
 
 
-trafego = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep= ';', encoding= 'latin')
-#filtro = tabela_utils.filtrar(trafego,['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
-#trafego.loc[trafego('ANO')<2015]
-print(trafego['ANO'])#nesta linha eu pego apenas a coluna ANO e imprimo
+dados = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep= ';', encoding= 'latin')
+mes_6 = tabela_utils.filtrar_linha(dados, 'MÊS', '6')
+mes_6_filtrados = tabela_utils.filtrar(mes_6, ['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
+mes_6_sem_nulos = tabela_utils.retirar_nulos(mes_6_filtrados)
+print(mes_6_sem_nulos)
 
