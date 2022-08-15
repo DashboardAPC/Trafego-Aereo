cabb92973b1f7fe65842c59fe1ded099823dd014
Hunter104 <pudimdeleite2004@outlook.com>
2022-08-15 17:48:31 -0300

    Adicionados logs e funçao utilitario

--- Dashboard-Oficial/src/mapa.py
+++ Dashboard-Oficial/src/mapa.py
@@ -4,15 +4,29 @@ import plotly.express as  px
 import json
 import tabela_utils
 
+
+def operacoes(tabela: pd.DataFrame,funcoes: dict) -> pd.DataFrame:
+    for function in funcoes:
+       tabela =  function(tabela, *funcoes[function])
+
+
+
+print('Lendo geometria...')
 estados_brasileiros = json.load(open('Dashboard-Oficial/data/brasil_estados.json'))
+print('Lendo dados...')
 voos = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep=';', encoding='latin')
 
-voos_por_estado = tabela_utils.filtrar(voos,['AEROPORTO DE DESTINO (UF)','DECOLAGENS'])
-vpe_nao_nulos = tabela_utils.retirar_nulos(voos_por_estado)
-decolagens_por_estado = tabela_utils.soma_por_categoria(vpe_nao_nulos, 'AEROPORTO DE DESTINO (UF)', 'DECOLAGENS')
-maximo_decolagens = tabela_utils.maximo(decolagens_por_estado, 'DECOLAGENS')
+print('Filtrando colunas...')
+voos = tabela_utils.filtrar(voos,['AEROPORTO DE DESTINO (UF)','DECOLAGENS'])
+print('Retirando valores nulos...')
+voos = tabela_utils.retirar_nulos(voos)
+print('Somando por categoria...')
+voos = tabela_utils.soma_por_categoria(voos, 'AEROPORTO DE DESTINO (UF)', 'DECOLAGENS')
+print('Calculando valor máximo...')
+maximo_decolagens = tabela_utils.maximo(voos, 'DECOLAGENS')
 
-mapa = px.choropleth(decolagens_por_estado,
+print('Produzindo mapa...')
+mapa = px.choropleth(voos,
                     template='plotly_dark',
                     geojson=estados_brasileiros, 
                     locations='AEROPORTO DE DESTINO (UF)', 
@@ -22,4 +36,6 @@ mapa = px.choropleth(decolagens_por_estado,
                     scope='south america',
                     color_continuous_scale='purp'
                     )
+
+print('Mostrando mapa...')
 mapa.show()
