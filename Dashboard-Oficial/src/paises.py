7569f000b9a6b717b7b91105aceb8b914740733f
animaniacs003 <analuisadantas03@gmail.com>
2022-08-15 16:30:43 -0300

    commit de novo

--- Dashboard-Oficial/src/paises.py
+++ Dashboard-Oficial/src/paises.py
@@ -2,7 +2,9 @@ import math
 import pandas as pd
 import tabela_utils
 
+
 trafego = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep= ';', encoding= 'latin')
 #filtro = tabela_utils.filtrar(trafego,['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
 #trafego.loc[trafego('ANO')<2015]
 print(trafego['ANO'])#nesta linha eu pego apenas a coluna ANO e imprimo
+
