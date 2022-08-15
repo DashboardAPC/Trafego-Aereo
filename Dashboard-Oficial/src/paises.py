d00bf5b1cfe143ab046dc5e92c9ae70bc4ce902d
animaniacs003 <analuisadantas03@gmail.com>
2022-08-15 15:18:09 -0300

    def filtrar

--- Dashboard-Oficial/src/paises.py
+++ Dashboard-Oficial/src/paises.py
@@ -2,9 +2,7 @@ import math
 import pandas as pd
 import tabela_utils
 
-
-tabela = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep=';', encoding='latin')
-filtro = tabela_utils.filtrar(tabela, ['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
-
-
-print(filtro.to_string())
+trafego = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep= ';', encoding= 'latin')
+#filtro = tabela_utils.filtrar(trafego,['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
+#trafego.loc[trafego('ANO')<2015]
+print(trafego['ANO'])#nesta linha eu pego apenas a coluna ANO e imprimo
