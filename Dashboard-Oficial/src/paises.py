c8281b30e230b0a9a31d7d03effd4ad70cbefb5f
Guilherme <guilhermecarmona10@gmail.com>
2022-08-16 09:39:12 -0300

    Filtrando os anos

--- Dashboard-Oficial/src/paises.py
+++ Dashboard-Oficial/src/paises.py
@@ -4,8 +4,15 @@ import tabela_utils
 
 
 dados = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep= ';', encoding= 'latin')
-mes_6 = tabela_utils.filtrar_linha(dados, 'MÊS', '6')
-mes_6_filtrados = tabela_utils.filtrar(mes_6, ['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
-mes_6_sem_nulos = tabela_utils.retirar_nulos(mes_6_filtrados)
-print(mes_6_sem_nulos)
+print('Filtrando colunas...')
+colunas_filtradas = tabela_utils.filtrar(dados, ['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
+print('Retirando nulos...')
+dados_sem_nulos = tabela_utils.retirar_nulos(colunas_filtradas)
+print('Filtrando anos...')
+dados_anos = tabela_utils.filtrar_linha(dados_sem_nulos, 'ANO', ['2013', '2014'])
+print('Filtrando mês...')
+mes_6 = tabela_utils.filtrar_linha(dados_anos, 'MÊS', ['6'])
 
+#filtrando a tabela eliminando todos os dados que não precisaremos
+
+print(mes_6)
