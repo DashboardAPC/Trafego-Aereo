af25c113cc5e28ef373be06b7804b390c57f1e08
animaniacs003 <analuisadantas03@gmail.com>
2022-08-18 18:42:17 -0300

    Gráfico refeito

--- Dashboard-Oficial/src/paises.py
+++ Dashboard-Oficial/src/paises.py
@@ -7,7 +7,7 @@ def faztudo(tabela : pd.DataFrame):
     tabela = tabela_utils.filtrar(tabela, ['DECOLAGENS'])
     tabela = (tabela['DECOLAGENS'].to_list())
     return tabela
-
+print('Lendo as informações...')
 dados = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep= ';', encoding= 'latin')
 print('Filtrando colunas...')
 colunas_filtradas = tabela_utils.filtrar(dados, ['ANO', 'MÊS', 'AEROPORTO DE ORIGEM (PAÍS)', 'DECOLAGENS'])
@@ -18,7 +18,7 @@ dados_anos = tabela_utils.filtrar_linha(dados_sem_nulos, 'ANO', ['2013', '2014']
 print('Filtrando mês...')
 mes_6 = tabela_utils.filtrar_linha(dados_anos, 'MÊS', ['6'])
 print('Filtrando países...')
-dados_paises = tabela_utils.filtrar_linha(mes_6, 'AEROPORTO DE ORIGEM (PAÍS)', ['ESTADOS UNIDOS DA AMÉRICA', 'MÉXICO', 'ARGENTINA', 'REINO UNIDO', 'EMIRADOS ÁRABES UNIDOS'] )
+dados_paises = tabela_utils.filtrar_linha(mes_6, 'AEROPORTO DE ORIGEM (PAÍS)', ['ESTADOS UNIDOS DA AMÉRICA', 'MÉXICO', 'ARGENTINA', 'CHILE', 'EMIRADOS ÁRABES UNIDOS'] )
 #print(dados_paises)
 print('Filtrando o ano de 2013...')
 dados_2013 = tabela_utils.filtrar_linha(dados_paises, 'ANO', ['2013'])
@@ -31,8 +31,10 @@ soma_2014 = tabela_utils.soma_por_categoria(dados_2014, 'AEROPORTO DE ORIGEM (PA
 
 ano2013 = faztudo(soma_2013)
 ano2014 = faztudo(soma_2014)
+print(soma_2013)
+print(soma_2014)
 
-aeroporto = ['ESTADOS UNIDOS DA AMÉRICA', 'MÉXICO', 'ARGENTINA', 'REINO UNIDO', 'EMIRADOS ÁRABES UNIDOS','ESTADOS UNIDOS DA AMÉRICA', 'ARGENTINA', 'MÉXICO', 'REINO UNIDO', 'EMIRADOS ÁRABES UNIDOS',]
+aeroporto = ['ESTADOS UNIDOS DA AMÉRICA', 'MÉXICO', 'ARGENTINA', 'CHILE', 'EMIRADOS ÁRABES UNIDOS','ESTADOS UNIDOS DA AMÉRICA', 'ARGENTINA', 'CHILE', 'MÉXICO', 'EMIRADOS ÁRABES UNIDOS',]
 decolagens = ano2013 + ano2014
 anos = ('2013',)*5 + ('2014',)*5
 
@@ -41,5 +43,10 @@ grafico = pd.DataFrame({
     "Decolagens": decolagens,
     "Ano": anos
 })
-fig = px.bar(grafico, x="Países", y="Decolagens", color="Ano", pattern_shape_sequence=[".", "x", "+"], color_discrete_sequence=px.colors.qualitative.Prism, template='plotly_dark', title='Voos por pais durante a copa')
+fig = px.histogram(grafico, x="Países", y="Decolagens",
+             color='Ano', barmode='group',
+             histfunc='avg',
+            title = 'Voos por país durante a copa',
+             template='plotly_dark')
 fig.show()
+
