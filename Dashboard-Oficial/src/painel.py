851d57338125b52188a75592d60023c34eda6afb
Ricardo <lrsj2003@gmail.com>
2022-09-05 10:03:49 -0300

    interatividade para pizza_tipo_carga e comentarios

--- Dashboard-Oficial/src/painel.py
+++ Dashboard-Oficial/src/painel.py
@@ -1,20 +1,25 @@
+# ----------------------------------------- Importando bibliotecas -----------------------------------------
 from dash import Dash, html, dcc, Output, Input
+
 from barras_data_pico import grafico_barras_data_pico
 from barras_paises_origem import grafico_barras_paises_origem
 from mapa import grafico_mapa
 from pizza_preferencia_empresa import grafico_pizza_preferencia_empresa
-from pizza_tipo_carga import grafico_pizza_tipo_carga
+from pizza_tipo_carga import cria_grafico_pizza_tipo_carga
 
 
+# ---------------------------------------------- Criando dash ----------------------------------------------
 app = Dash(__name__)
 
 
+# ---------------------------------- Definindo constantes de customização ----------------------------------
 cores_texto = {
     'fundo_texto': '#111111',
     'texto': '#7FDBFF'
 }
 
 
+# --------------------------------------------- Criando layout ---------------------------------------------
 app.layout = html.Div([
 
     # -------------------------- Titulos --------------------------
@@ -134,28 +139,40 @@ app.layout = html.Div([
             ),
             dcc.Graph(
                 id = 'grafico_pizza_tipo_carga',
-                figure = grafico_pizza_tipo_carga
+                figure = cria_grafico_pizza_tipo_carga(['2013', '2014', '2015'])
             )
         ]
     )
 ])
 
-# -------------------------------------------------------- Callbacks --------------------------------------------------------
+
+# -------------------------------------- Interatividade Quinto Grafico --------------------------------------
 @app.callback(
     Output(component_id = 'titulo_grafico_pizza_tipo_carga', component_property = 'children'),
     Input(component_id = 'filtro_ano', component_property = 'value')
 )
-def interatividade_pizza_tipo_carga(value):
+def interatividade_titulo_pizza_tipo_carga(value): # Muda os anos no titulo html do gráfico
     if value == []:
-        ano = '2013, 2014 e 2015'
+        anos = '2013, 2014 e 2015'
     elif len(value) == 1:
-        ano = value[0]
+        anos = value[0]
     else:
         value = sorted(value)
-        ano = str(", ".join(value[:-1])) + ' e ' + str(value[-1])
-    return f'Percentual de peso transportado pelos aviões no Brasil em {ano}'
+        anos = str(", ".join(value[:-1])) + ' e ' + str(value[-1])
+    return f'Percentual de peso transportado pelos aviões no Brasil em {anos}'
+
+@app.callback(
+    Output(component_id = 'grafico_pizza_tipo_carga', component_property = 'figure'),
+    Input(component_id = 'filtro_ano', component_property = 'value')
+)
+def interatividade_grafico_pizza_tipo_carga(value): # Muda os valores que serão considerados na criação do gráfico
+    anos = [float(item) for item in value] # TODO: Essa adaptacao horrivel pode ser removida se descobrirmos pq filtrar nulos esta transformando os numeros em float
+    anos = [str(item) for item in anos]
+    if value == []: # No caso especifico do usuario limpar o Dropdown deve mostrar por padrao de todos os anos
+        anos = ['2013.0', '2014.0', '2015.0']
+    return cria_grafico_pizza_tipo_carga(anos)
 
 
-
+# ------------------------------------------ Colocando dash no ar ------------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
