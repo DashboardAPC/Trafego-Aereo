a7d429261fe083799a4b55cc8bc9685c0ad41e14
Hunter104 <pudimdeleite2004@outlook.com>
2022-09-14 16:23:58 -0300

    Adicionados caminhos interoperacionais

--- Dashboard-Oficial/src/barras_paises_origem.py
+++ Dashboard-Oficial/src/barras_paises_origem.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import plotly.express as px
 import tabela_utils
-
+from pathlib import Path
 
 # ------------------------------------------- Funções Específicas -------------------------------------------
 def faztudo(tabela : pd.DataFrame):
@@ -10,10 +10,10 @@ def faztudo(tabela : pd.DataFrame):
     tabela = (tabela['DECOLAGENS'].to_list())
     return tabela
 
-
+data = Path('Dashboard-Oficial/data')
 # ---------------------------------------------- Lendo dataset ----------------------------------------------
 print('2 - Lendo dataset...') # Feedback
-dados = pd.read_csv('Dashboard-Oficial\data\ANAC20XX-13-14-15.csv', sep = ';', encoding = 'latin') # Encoding resolve problema da acentuação
+dados = pd.read_csv(data / 'ANAC20XX-13-14-15.csv', sep = ';', encoding = 'latin') # Encoding resolve problema da acentuação
 
 
 # --------------------------------------- Manipulando dados necessarios---------------------------------------
